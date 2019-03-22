53e6719ac36e0fad31672daa7d53bac94bb59165
USER <USER@AMP-11>
2019-03-22 15:59:40 +0900

    commit

--- chap04-ex07/src/Book.java
+++ chap04-ex07/src/Book.java
@@ -1,8 +1,11 @@
 
-public class Book {
-	String title. author;
-	public Book(String title, String author) {
-		this.title = title;
-		this.author = author;
+public class Book { 
+ 	String title, author; 
+ 
+ 
+ 	public Book(String title, String author) { 
+ 		this.title = title; 
+	this.author = author; 
 	}
-}
+ 
+} 
--- chap04-ex07/src/BookArrey.java
+++ chap04-ex07/src/BookArrey.java
@@ -1,24 +1,32 @@
-import java.util.Scanner;
-
-public class BookArrey {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Book [] book = new Book[2];
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		for(int i=0;i<book.length; i++){
-			System.out.print("title : ");
-			String title = scanner.nextLine();
-			System.out.print("Author : ");
-			String author = scanner.nextLine();
-			book[1] = new Book(title, author);
-		}
-		for(int i=0; i<book.length;i++)
-			System.out.println(book[i].title + " " + book[i].author);
-		scanner.close();
-	}
-	
-
-}
+import java.util.Scanner; 
+ 
+ 
+ public class BookArrey { 
+ 
+ 
+ 	public static void main(String[] args) { 
+ 		// TODO Auto-generated method stub 
+ 		Book[] book = new Book[2]; 
+ 
+ 
+ 		Scanner scanner = new Scanner(System.in); 
+ 
+ 
+ 		for (int i = 0; i < book.length; i++) { 
+ 			System.out.print("Title : "); 
+ 			String title = scanner.nextLine(); 
+ 			System.out.print("Author : "); 
+ 			String author = scanner.nextLine(); 
+ 			book[i] = new Book(title, author); 
+ 		} 
+ 
+ 
+ 		for (int i = 0; i < book.length; i++) 
+ 			System.out.println(book[i].title + "  " + book[i].author); 
+ 		scanner.close(); 
+ 
+ 
+ 	} 
+ 
+ 
+ } 
