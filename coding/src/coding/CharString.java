d1207da049ccbfdb7fb65104db39c66b0394ffb1
USER <USER@AMP-11>
2019-03-22 16:04:38 +0900

    commit

---
+++ coding/src/coding/CharString.java
@@ -0,0 +1,16 @@
+package coding;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('생');
+		System.out.println("생활코딩");
+		System.out.println("생");
+		System.out.println("생활코딩"+" 입니다");
+		System.out.println(1+1);
+		System.out.println("1"+"1");
+		System.out.println("egoing said\n\"Welcome\nprogramming world\"");
+
+	}
+
+}
---
+++ coding/src/coding/Number.java
@@ -0,0 +1,13 @@
+package coding;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(2*5);
+		System.out.println(6/2);
+
+	}
+
+}
